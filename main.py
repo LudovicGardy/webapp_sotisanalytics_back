6b4371ce282224c94a003321d2337ab86745ecec
Ludovic Gardy <ludovic.gardy@gmail.com>
2023-11-26 17:17:41 +0100

    test origins

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 ### Cross-origin
 # CORS(app) # This line is important to allow our server to accept cross-origin requests
 # CORS(app, origins=['http://localhost:3000']) # Limit the cross-origin requests to the specified origins, which can be a good practice for security once you have deployed your app in production.
-CORS(app, resources={r"/*": {"origins": "*"}}) # Allow all origins    
+# CORS(app, resources={r"/*": {"origins": "*"}}) # Allow all origins    
 
 ### Connexion to the backend might not work if you run the react app in localhost.
 ### But once deployed, it works.
