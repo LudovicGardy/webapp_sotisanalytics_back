23b41f838087c5917c46cc1dcf1db9929844b996
Ludovic Gardy <ludovic.gardy@gmail.com>
2023-11-26 16:13:46 +0100

    added var env

--- main.py
+++ main.py
@@ -1,74 +1,35 @@
 #conda activate flask_env
+import numpy
+import platform
 
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
-import numpy
-from dotenv import load_dotenv, dotenv_values
-import os
 from firebase_admin import credentials, initialize_app, firestore
 from datetime import datetime
-import platform
 
-# Utilisez un fichier d'informations d'identification pour initialiser Firebase
+from modules.config import firebase_credentials
+firebase_cred = firebase_credentials()
+cred = credentials.Certificate(firebase_cred)
 # cred = credentials.Certificate('firestore-key.json')
 
-try:
-    # Chargez les variables d'environnement à partir du fichier .env
-    config = dotenv_values(".env")
-    cred_dict = {
-    "type": "service_account",
-    "project_id": config['PROJECT_ID'],
-    "private_key_id": config['PRIVATE_KEY_ID'],
-    "private_key": f"-----BEGIN PRIVATE KEY-----\n{config['PRIVATE_KEY']}\nPLM7lHi2Xo7i+CloZ8KOzwaS0KV+3xdtwFUDOIXO3BVLQdror6ouWlAw08TCtFC/\ns9II6ugraSWUZxV/O1qKxHca45meUXd/AzJusQA0JOmW+k7Ocm7Ym27HzTccXPi7\n7MABSsOKzBpiWa8HqeOKPk1yjupy8AzLa5OH9YaY0Dyq1aKj7N3Su6MhRu+ucUUm\nfifwTlUXTEWeJHtYvKj/Utr6ViZZG4s8ghozI7ShebBACzS5K1Vomlvb4rISVFt0\nhxMvujZaWDtRG17PEQLR8dzzExV8ZSbO60WrV9EycJEen6c2FeYjM6Nbi6X7/pzN\nUTYotzzbAgMBAAECggEAHGKdG6d3848UEqCNNM1yHZAnu5v+QeiMQmPOekfLiV3i\nQG5c1b4Twqs6H7zU1jZNv07sdEGxydIa75Jvr0uxRuu3FnCPogrl3bpVxFzVTnEZ\nUirLnY+h1AnMcD13g/3g5LbespsyT0ZGssNTUGoHHaSAeCDdvvQYyLiJMGekvy/P\n/6/01LIbb+Kdc0Gx544xn/FoSpezvh7t0EqbWSM1Nptz+WGRQ5LPBRVEBrc5QcPe\npctBeSweMhvSuycWlG2tmhofgu0T30wYPgeyPP2+uzBH6+3HaqaRD6mucxq/L0hB\nkcx1kvpviLNUM+gLFwTWUfO80ObeIaSsbNrBkVzjYQKBgQDSn9RhUOrK/UZJzXN6\nEVjwx3rFKVwPLuag2YiHuWdTnQswGeJJjIDh2mcldHWn6MGSuZu6FdP+rSbcf1xw\nzRDVnaPQuVnT1GjIPKPtglCkxeU70lx/0lTX75dCquDmqZXMVkf46pvDQGdrBpCF\ngyhRvKIHfevznI1G6qwKFH1JeQKBgQC6CIrtMuTRDMXIHAr5qPHXmVKjqn1tKStF\nEe6ZL07WzsmEoKlDbLCqSkfQvJ1NrZ8Bs68UUhqaZjCaZgf+cg8VF5icTNvhuv4/\nIdCFlXwxuTQ08tnuBGb+paJ3o0oauo6hPenfyN1dii1GIDnz7XwbJTAc7SzMGeAq\n7TxwHC238wKBgCC2qGLxEXazve4Klgv4k8raAMyMrvrAuxtyjg0ek9jdxHYVHxtz\nUjVCGdEsdHW+5gnnADP33fRpambG9VGj2CCFmoL5tuT60cd/+6oRGnttLTyMYMeN\noJXlZaX6KnJJFrYlFIqpzcWWxDlQTLQf+ewwEy58tWAiCaNhIZVzNz5pAoGAJ7qj\nULzJuQttQasbfO3jmBOaXnGOj713DC0kM3qy75UB3F0jSM7xe6yZYa6mOWyWxJpH\nDaPrIoYoYxDfLCvXpL2BTf/sBW0V1w21ppGiEExpJdWnTPmLtV5SMBKjRMoKo8zB\nIZWYEN3thUhkl/9jvbhXahYMtxnfkctQWPDropkCgYBKqYctlRbQjNbs5tODL1CY\nZ8ittP1q9WGG49KvqyavfKXnFvdMT8YBbxgb9HXm2udOVoilCT0O/kWo5mqXa0G3\ndzCEgsC+0IIelp8gxkp+NDt68Aj30FAxRA995ypHIz0ZaRB2d+KA1XIYuzWTkuf6\nQ7zSmByx8VarrC2lwWSgHA==\n-----END PRIVATE KEY-----\n",
-    "client_email": config['CLIENT_EMAIL'],
-    "client_id": config['CLIENT_ID'],
-    "auth_uri": config['AUTH_URI'],
-    "token_uri": config['TOKEN_URI'],
-    "auth_provider_x509_cert_url": config['AUTH_PROVIDER_X509_CERT_URL'],
-    "client_x509_cert_url": config['CLIENT_X509_CERT_URL']
-    }
-except Exception as e:
-    # Récupérez les valeurs à partir des variables d'environnement
-    print("No file .env was found :", str(e))
-    print("Searching in OS environment variables...")
-    # load_dotenv()
-    cred_dict = {
-        "type": "service_account",
-        "project_id": os.environ.get('PROJECT_ID'),
-        "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
-        "private_key": f"-----BEGIN PRIVATE KEY-----\n{os.environ.get('PRIVATE_KEY')}\nPLM7lHi2Xo7i+CloZ8KOzwaS0KV+3xdtwFUDOIXO3BVLQdror6ouWlAw08TCtFC/\ns9II6ugraSWUZxV/O1qKxHca45meUXd/AzJusQA0JOmW+k7Ocm7Ym27HzTccXPi7\n7MABSsOKzBpiWa8HqeOKPk1yjupy8AzLa5OH9YaY0Dyq1aKj7N3Su6MhRu+ucUUm\nfifwTlUXTEWeJHtYvKj/Utr6ViZZG4s8ghozI7ShebBACzS5K1Vomlvb4rISVFt0\nhxMvujZaWDtRG17PEQLR8dzzExV8ZSbO60WrV9EycJEen6c2FeYjM6Nbi6X7/pzN\nUTYotzzbAgMBAAECggEAHGKdG6d3848UEqCNNM1yHZAnu5v+QeiMQmPOekfLiV3i\nQG5c1b4Twqs6H7zU1jZNv07sdEGxydIa75Jvr0uxRuu3FnCPogrl3bpVxFzVTnEZ\nUirLnY+h1AnMcD13g/3g5LbespsyT0ZGssNTUGoHHaSAeCDdvvQYyLiJMGekvy/P\n/6/01LIbb+Kdc0Gx544xn/FoSpezvh7t0EqbWSM1Nptz+WGRQ5LPBRVEBrc5QcPe\npctBeSweMhvSuycWlG2tmhofgu0T30wYPgeyPP2+uzBH6+3HaqaRD6mucxq/L0hB\nkcx1kvpviLNUM+gLFwTWUfO80ObeIaSsbNrBkVzjYQKBgQDSn9RhUOrK/UZJzXN6\nEVjwx3rFKVwPLuag2YiHuWdTnQswGeJJjIDh2mcldHWn6MGSuZu6FdP+rSbcf1xw\nzRDVnaPQuVnT1GjIPKPtglCkxeU70lx/0lTX75dCquDmqZXMVkf46pvDQGdrBpCF\ngyhRvKIHfevznI1G6qwKFH1JeQKBgQC6CIrtMuTRDMXIHAr5qPHXmVKjqn1tKStF\nEe6ZL07WzsmEoKlDbLCqSkfQvJ1NrZ8Bs68UUhqaZjCaZgf+cg8VF5icTNvhuv4/\nIdCFlXwxuTQ08tnuBGb+paJ3o0oauo6hPenfyN1dii1GIDnz7XwbJTAc7SzMGeAq\n7TxwHC238wKBgCC2qGLxEXazve4Klgv4k8raAMyMrvrAuxtyjg0ek9jdxHYVHxtz\nUjVCGdEsdHW+5gnnADP33fRpambG9VGj2CCFmoL5tuT60cd/+6oRGnttLTyMYMeN\noJXlZaX6KnJJFrYlFIqpzcWWxDlQTLQf+ewwEy58tWAiCaNhIZVzNz5pAoGAJ7qj\nULzJuQttQasbfO3jmBOaXnGOj713DC0kM3qy75UB3F0jSM7xe6yZYa6mOWyWxJpH\nDaPrIoYoYxDfLCvXpL2BTf/sBW0V1w21ppGiEExpJdWnTPmLtV5SMBKjRMoKo8zB\nIZWYEN3thUhkl/9jvbhXahYMtxnfkctQWPDropkCgYBKqYctlRbQjNbs5tODL1CY\nZ8ittP1q9WGG49KvqyavfKXnFvdMT8YBbxgb9HXm2udOVoilCT0O/kWo5mqXa0G3\ndzCEgsC+0IIelp8gxkp+NDt68Aj30FAxRA995ypHIz0ZaRB2d+KA1XIYuzWTkuf6\nQ7zSmByx8VarrC2lwWSgHA==\n-----END PRIVATE KEY-----\n",
-        "client_email": os.environ.get('CLIENT_EMAIL'),
-        "client_id": os.environ.get('CLIENT_ID'),
-        "auth_uri": os.environ.get('AUTH_URI'),
-        "token_uri": os.environ.get('TOKEN_URI'),
-        "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
-        "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL')
-    }
-
-# Créez un objet credentials à partir du dictionnaire
-cred = credentials.Certificate(cred_dict)
-# cred = credentials.Certificate('firestore-key.json')
-
-# Initialisez l'application
+# App initialization
 initialize_app(cred)
-
 db = firestore.client()
-
 app = Flask(__name__)
 
 ### Cross-origin
-CORS(app)  # Cette ligne est importante pour permettre à notre serveur d'accepter les requêtes cross-origin
-# CORS(app, origins=['http://localhost:3000']) # Limite les requêtes cross-origin aux origines spécifiées, ce qui peut être une bonne pratique pour la sécurité une fois que vous avez déployé votre application en production.
+CORS(app) # This line is important to allow our server to accept cross-origin requests
+# CORS(app, origins=['http://localhost:3000']) # Limit the cross-origin requests to the specified origins, which can be a good practice for security once you have deployed your app in production.
 
-### Note: la connexion au backend ne fonctionne pas si je lance 
-### L'appli react en localhost. Mais une fois déployée, ça fonctionne.
 
+### Connexion to the backend might not work if you run the react app in localhost.
+### But once deployed, it works.
 @app.route('/', methods=['GET'])
 def hello():
     return "Hello World !"
 
 def check_origin(origin):
-    # Liste des origines autorisées
+    # List of allowed origins
     allowed_origins = [
         "http://www.sotisanalytics.com",
         "https://www.sotisanalytics.com",
@@ -80,7 +41,7 @@ def check_origin(origin):
         "https://sotisanalytics.fr"
     ]
 
-    # Vérifiez si l'origine est dans la liste des origines autorisées
+    # Check if the origin is allowed
     if origin in allowed_origins or platform.node() == "MacBookPro-LudovicGardy.local":
         allow = True
         print(f"Success: <{origin}>.")
@@ -93,24 +54,24 @@ def check_origin(origin):
 @app.route('/api/submitTestimonial', methods=['POST'])
 def submit_testimonial():
 
-    # Obtenez l'origine de la requête
+    # Get the origin of the request
     origin = request.headers.get('Origin')
     allow = check_origin(origin)
     if not allow:
-        abort(403)  # Forbidden
+        abort(403) # Forbidden
 
     data = request.get_json()
     name = data["name"]
     email = data["email"]
     comment = data["comment"]
 
-    # Obtenez la date et l'heure actuelles au format souhaité
+    # Get the current date and time
     date_hour = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    # Créez une référence à la sous-collection basée sur la date et l'heure
+    # Create a reference to the document
     doc_ref = db.collection('testimonials_submited').document(date_hour)
 
-    # Ajoutez les données dans la sous-collection
+    # Add data to the document
     doc_ref.set({
         'name': name,
         'email': email,
@@ -128,11 +89,11 @@ def submit_testimonial():
 @app.route('/api/getData', methods=['GET'])
 def get_data():
     
-    # Récupérez les données de Firestore
+    # Get data from the database
     data_ref = db.collection('testimonials')
     data = data_ref.get()
 
-    # Transformez les données en format JSON
+    # Transform data into a list of dictionaries
     result = [item.to_dict() for item in data]
 
     print("----")
@@ -145,4 +106,4 @@ if __name__ == '__main__':
 
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='0.0.0.0', port=8000, debug=True) # Si Docker
+    app.run(host='0.0.0.0', port=8000, debug=True) # If Dockerised
---
+++ modules/config.py
@@ -0,0 +1,51 @@
+import numpy as np
+import os
+from dotenv import load_dotenv, find_dotenv
+
+def load_configurations():
+    """
+    Charge uniquement les variables du fichier .env si celui-ci est présent.
+    Si le fichier .env n'existe pas, charge toutes les variables d'environnement du système.
+    """
+    dotenv_path = find_dotenv('.env')
+
+    if dotenv_path:
+        # Le fichier .env existe, charger uniquement ses variables
+        load_dotenv(dotenv_path)
+        # Retourne les variables chargées depuis le .env
+        return {key: os.environ[key] for key in os.environ if key in open(dotenv_path).read()}
+    else:
+        # Le fichier .env n'existe pas, retourne toutes les variables d'environnement du système
+        return dict(os.environ)
+
+### CREDENTIALS ###
+def firebase_credentials():
+    '''
+    Load configuration from .env file or from OS environment variables
+    '''
+    
+    # List of required keys in lowercase
+    keys_list = [
+        'project_id', 'private_key_id', 'private_key', 'client_email',
+        'client_id', 'auth_uri', 'token_uri', 'auth_provider_x509_cert_url',
+        'client_x509_cert_url'
+    ]
+    
+    cred_dict = {}
+    env_variables = load_configurations()
+
+    # Check if all required keys exist and have a non-empty value
+    try:
+        for key in keys_list:
+            value = env_variables.get(key.upper())
+            if not value:
+                raise ValueError(f'Missing or empty value for key: {key}')
+            cred_dict[key] = value
+
+        # Add prefix and suffix for the private_key
+        cred_dict['private_key'] = cred_dict["private_key"].replace("/breakline/", "\n")
+    except ValueError as e:
+        print(f'Configuration error: {e}')
+        cred_dict = {}  # Reset cred_dict if any key is missing or empty
+
+    return cred_dict
